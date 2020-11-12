3fbe4347ca1bdcb584e175ecb35ed68b82d8dc62
Colin Dean <git@cad.cx>
2020-11-12 11:35:16 -0500

    [WIP] Add zip resolution

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #![deny(warnings)]
 use std::path::Path;
 use std::fs::File;
-use mini_fs::zip::ZipFs;
+//use mini_fs::zip::ZipFs;
 use warp::Filter;
+//use mini_fs::prelude::*;
+use mini_fs::{MiniFs, ZipFs, Store};
 
 #[tokio::main]
 async fn main() {
 
     let ziparg = get_zippath().expect("no zipfile specified");
     let zippath = Path::new(&ziparg);
-    let _zipfs = get_zipfs(zippath)
-        .map(|z| z.index())
-        .expect("zipfile {} not found");
+    let zipfs = get_zipfs(zippath)
+        .map(|z| z.index().expect("unable to index zipfile"))
+        .expect("zipfile not found");
 
+    let minifs = MiniFs::new().mount("zip", zipfs);
+        
     let readme = warp::get()
         .and(warp::path::end())
         .and(warp::fs::file("./README.md"));
@@ -22,7 +26,16 @@ async fn main() {
 
     let zip = warp::get()
         .and(warp::path("zip"))
-        .map(|| "zip");
+        .map(|| 
+            get_zipentries_at_path(&minifs, Path::new("/zip/")).map_or_else("", 
+            |files|
+                files
+                .iter()
+                .map(|osstr| osstr.into_string().unwrap())
+                .collect()
+                .join("\n")
+        )
+        );
 //        .and(zipfs.entries());
 
     // GET / => README.md
@@ -46,4 +59,22 @@ use std::env;
 fn get_zippath() -> Option<String> {
     let args = env::args().collect::<Vec<String>>();
     args.first().map(|a| String::from(a))
+}
+
+use std::io;
+
+fn get_zipentries_at_path(minifs: &MiniFs, path: &Path) -> io::Result<Vec<String>> {
+    minifs
+            .entries_path(Path::new("/zip/"))
+            .map(|list|
+                list
+                    .map(|maybeEntry|
+                        maybeEntry.map(|entry|
+                            entry.name.into_string()
+                        )
+                    )
+                    .collect()
+
+            )
+            .map(|fd| fd.collect())
 }
